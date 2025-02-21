b79b14108afd7f5fcbe800bfca08e1f94b9d3d3f
Sheetu1 <sheetusharma548@gmail.com>
2025-02-21 11:47:42 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./src/app');
+
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+io.on('connection', Socket => {
+console.log('user connected');
+
+});
+
+server.listen(3000,() => {
+    console.log('App is running on port 3000');
+});
+
